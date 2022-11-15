3954417bb4a54386e0a3943b5258675c04534f92
cruby9 <cruby9@yahoo.com>
2022-11-15 10:22:18 -0700

    first commit

---
+++ age.rb
@@ -0,0 +1,4 @@
+puts "How old are you?"
+age = gets.chomp
+puts "In 10 years you will be:"
+puts age.to_i + 10
---
+++ name.rb
@@ -0,0 +1,5 @@
+puts "Whats your first name?"
+first_name = gets.chomp
+puts "Whats your last name?"
+last_name = gets.chomp
+puts first_name +  last_name
